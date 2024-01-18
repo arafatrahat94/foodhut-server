ffc9243c47e5b6d6ac430ba99c44630da6f8d643
arafatrahat94 <arafatrahat94@gmail.com>
2024-01-18 15:48:38 +0600

    added route api for todays offer with pagination process

--- TodaysOffer.js
+++ TodaysOffer.js
@@ -9,4 +9,17 @@ TodaysOfferRoutes.get("/Offers", async (req, res) => {
   const result = await TodaysOfferCollection.find().toArray();
   res.send(result);
 });
+TodaysOfferRoutes.get("/OfferedProducts", async (req, res) => {
+  console.log(req.query);
+  const page = parseInt(req.query.page) || 0;
+  const limit = parseInt(req.query.limit);
+  const skip = page * limit;
+  const foodCount = await TodaysOfferCollection.countDocuments();
+  const result = await TodaysOfferCollection.find()
+    .skip(skip)
+    .limit(limit)
+    .toArray();
+  // console.log(result);
+  res.send({ count: foodCount, data: result });
+});
 module.exports = TodaysOfferRoutes;
